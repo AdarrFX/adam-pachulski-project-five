39fd7adced1c5f51c921c22e4f2f6d3c624fed09
AdarrFX <grenador@gmail.com>
2019-06-03 12:30:43 -0400

    Updates

---
+++ precache-manifest.f852345292fe619ee0b935e0ad884958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d986d64ac3e76f2879d21bd98a03540",
+    "url": "/adam-pachulski-project-five/index.html"
+  },
+  {
+    "revision": "bbc7546dc9a2885c6e33",
+    "url": "/adam-pachulski-project-five/static/css/main.b9958f5f.chunk.css"
+  },
+  {
+    "revision": "60d5ad77caa02facd9c4",
+    "url": "/adam-pachulski-project-five/static/js/2.4477133f.chunk.js"
+  },
+  {
+    "revision": "bbc7546dc9a2885c6e33",
+    "url": "/adam-pachulski-project-five/static/js/main.98e71fa7.chunk.js"
+  },
+  {
+    "revision": "791dac651cd9e0ebbfc5",
+    "url": "/adam-pachulski-project-five/static/js/runtime~main.7f7aca1e.js"
+  }
+]);
